68fc63346adb3071de31bd9ea5698f1458d18410
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-03-22 18:41:23 +0600

    V2 Surah Min

--- Compressor/compressed/divider.py
+++ Compressor/compressed/divider.py
@@ -5,23 +5,20 @@ import os
 # contents = open('singleSurah.min.json', encoding="utf8")
 # load = json.load(contents)
 data = requests.get(
-    'https://cdn.jsdelivr.net/gh/nhridoy/quran-api@main/v1/singleSurah.min.json').json()
+    'https://cdn.jsdelivr.net/gh/nhridoy/quran-api@main/v2/singleSurah.min.json').json()
 
 # print(type((load)))
 
 for key in data['singleSurah']:
     surah = data['singleSurah'][key]
-    dumping = str.encode(json.dumps(surah)).decode('unicode_escape')
+    dumping = json.dumps(surah, ensure_ascii=False)
     # print(dumping)
-    # print(type(dumping))
-    # break
-    # print(type(surah))
     # break
     newname = f'{key}.min.json'
-    new_file_location = os.path.join('divide/', newname)
-    write_file = open(new_file_location, 'wb')
-    write_file.write(dumping.encode('cp1252', errors='ignore'))
+    new_file_location = os.path.join('./divide/', newname)
+    write_file = open(new_file_location, 'w',  encoding="utf8")
+    # write_file.write(dumping.encode('cp1252', errors='ignore'))
 
     # newfile = str.encode(str(surah)).decode('unicode_escape')
-    # write_file.write(newfile)
-    # write_file.close()
+    write_file.write(dumping)
+    write_file.close()
